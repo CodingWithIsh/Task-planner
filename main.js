b103bcf6a38192a5194db5d71b5ceb8d52e9332c
Gitish98 <ismailiswell@gmail.com>
2021-05-21 17:48:31 +0100

    edits

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ if(!localStorage.getItem("submittedTasks")){
     localStorage.setItem("submittedTasks","{}")
     localStorage.setItem("IDs","0")
 }
+
 let tasks = JSON.parse(localStorage.getItem("submittedTasks"));
 let taskNumber = JSON.parse(localStorage.getItem("IDs"));
 let postits = 1
@@ -39,17 +40,18 @@ function fillPostItNote(task, postits){
     postItNote.id = task.id
     console.log(postItNote)
     postItNote.innerHTML = `              
-        <a href="#" contenteditable>
+        <a href="#">
         <button style=" position: absolute; margin-left: 6.9em;  background:transparent; width:30px; border-color:transparent;"onclick="deleteTask(${task.id})"><img src="redx.png" style="width:100%;"></button>
         <h2>Task ${postits}</h2>  
+        <button  onclick="edit(${task.id})"style="position: absolute; margin-bottom:1em; margin-left: 6.9em;  background:transparent; width:30px; border-color:transparent;"><img style = "width:100%;" src="edit.png"></button>
         <p>Name: ${task.name}<br>
+        <button  onclick="finished(${task.id})" style="position: absolute; margin-bottom:0.5em; margin-left: 7.2em; background:transparent; width:40px; border-color:transparent;"><img style = "width:100%;" src="tick.png"></button>
         Due date: ${task.duedate}<br>
         Assigned to: ${task.assigned}<br>
         Status: ${task.taskStatus}<br></p>
-        <p id="desc">Description: ${task.description}</p>
+        <p contenteditable id="desc">Description: ${task.description}</p>       
         </a>
     `
-
 
 
     if(postits<=8){document.getElementById("stickynotes").appendChild(postItNote);
@@ -60,6 +62,58 @@ function fillPostItNote(task, postits){
     
 }
 
+
+}
+
+function finished(taskId){
+    task=tasks[taskId]
+    document.getElementById(task.id).innerHTML = `        
+    <img src="finished.png" style="position:absolute; justify-content:center; z-index:2; width:200px;">      
+ <a href="#" style="opacity:40%">
+ <button style=" position: absolute; margin-left: 6.9em;  background:transparent; width:30px; border-color:transparent;"onclick="deleteTask(${task.id})"><img src="redx.png" style="width:100%;"></button>
+ <h2>Task COMPLETE</h2>
+ <p contenteditable>Name:COMPLETE<br>
+ Due date: COMPLETE<br>
+ Assigned to: COMPLETE<br>
+ Status: COMPLETE<br></p>
+ <p id="desc" content editable>Description: COMPLETE</p>
+ </a>
+`
+
+
+}
+
+function save(taskId){
+    task = tasks[taskId]
+    localStorage[taskId] = document.getElementById(taskId).innerHTML;
+    document.getElementById(task.id).innerHTML = `              
+    <a href="#">
+    <button style=" position: absolute; margin-left: 6.9em;  background:transparent; width:30px; border-color:transparent;"onclick="deleteTask(${task.id})"><img src="redx.png" style="width:100%;"></button>
+    <h2>Task ${postits}</h2>
+    <button style="position: absolute; margin-bottom:1em; margin-left: 6.9em;  background:transparent; width:40px; border-color:transparent;" onclick="edit(${task.id})"><img style = "width:100%;" src="edit.png"></button>
+    <p>Name: ${task.name}<br>
+    Due date: ${task.duedate}<br>
+    Assigned to: ${task.assigned}<br>
+    Status: ${task.taskStatus}<br></p>
+    <p id="desc">Description: ${task.description}</p>
+    </a>
+`
+}
+
+function edit(taskId){
+task = tasks[taskId]
+document.getElementById(task.id).innerHTML = `              
+ <a href="#">
+ <button style=" position: absolute; margin-left: 6.9em;  background:transparent; width:30px; border-color:transparent;"onclick="deleteTask(${task.id})"><img src="redx.png" style="width:100%;"></button>
+ <h2>Task ${postits}</h2>
+ <button style="position: absolute; margin-bottom:1em; margin-left: 6.9em;  background:transparent; width:40px; border-color:transparent;" onclick="save(${task.id})"><img style = "width:100%;" src="save.png.png"></button>
+ <p contenteditable>Name: ${task.name}<br>
+ Due date: ${task.duedate}<br>
+ Assigned to: ${task.assigned}<br>
+ Status: ${task.taskStatus}<br></p>
+ <p contenteditable id="desc" content editable>Description: ${task.description}</p>
+ </a>
+`
 }
 
 function updateLocalStorage(){
@@ -67,8 +121,10 @@ function updateLocalStorage(){
     localStorage.setItem('IDs', JSON.stringify(taskNumber))
 }
 
-function validateForm() {
-
+function clearAllTasks() {
+    localStorage.clear()
+    document.getElementById("stickynotes").innerHTML = ""
+    document.getElementById("stickynotes2").innerHTML = ""
 }
 
 function clearAllInputs(){
